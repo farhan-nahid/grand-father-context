89b31721d0bac3acd21d4f819b625a8d6d73d366
gesora96 <nahidahmed9933@gmail.com>
2021-10-27 02:34:05 +0600

    Grany is Angry��‍♀️

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { createContext, useState } from 'react';
 import './App.css';
+import GrandFather from './components/GrandFather/GrandFather';
+
+export const RingContext = createContext('ring');
 
 function App() {
-  return <div className='App'></div>;
+  const [house, setHouse] = useState(2);
+  const ornaments = 'Shokto Mati Ring';
+  return (
+    <RingContext.Provider value={[ornaments, house]}>
+      <div className='App'>
+        <button onClick={() => setHouse(house + 1)}>Buy a new house</button>
+
+        <GrandFather house={house}></GrandFather>
+      </div>
+    </RingContext.Provider>
+  );
 }
 
 export default App;
